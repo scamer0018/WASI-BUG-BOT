bdecc44b7d093d532b99e89a3c11667a3080502c
scamer0018 <scamer0018@gmail.com>
2024-12-31 01:49:34 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "919339344963"
+global.ownername = "S C A M E R "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
-global.location = "Pakistan, Lahore"
+global.location = "INDIA, BASIRHAT"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '919339344963'  //creator number
+global.ownername = ' S C A M E R ' //owner name
+global.botname = 'SCAMER BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
@@ -32,7 +32,7 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 
